c071bc34a404e3337735aec1ee6c9c8b84d20205
arjuna46 <147571007+arjuna46@users.noreply.github.com>
2023-11-15 19:57:55 +0700

    Update pratikum4.py

--- pratikum4.py
+++ pratikum4.py
@@ -1,32 +1,42 @@
+def hitung_nilai_akhir(tugas, uts, uas):
+    return round(0.3 * tugas + 0.35 * uts + 0.35 * uas, 1)
+
+# Inisialisasi list untuk menyimpan data
 data_mahasiswa = []
 
-def hitung_nilai_akhir(tugas, uts, uas):
-    nilai_akhir = 0.3 * tugas + 0.35 * uts + 0.35 * uas
-    return nilai_akhir
-
+# Input data menggunakan perulangan
 while True:
+    print("\nMasukkan data mahasiswa:")
+    nama = input("Nama        : ")
+    nim = int(input("NIM         : "))
+    nilai_tugas = int(input("Nilai Tugas : "))
+    nilai_uts = int(input("Nilai UTS   : "))
+    nilai_uas = int(input("Nilai UAS   : "))
 
-    nama = input("Masukkan Nama Mahasiswa: ")
-    nim = float(input("Masukkan Nim:"))
-    tugas = float(input("Masukkan Nilai Tugas: "))
-    uts = float(input("Masukkan Nilai UTS: "))
-    uas = float(input("Masukkan Nilai UAS: "))
+    # Hitung nilai akhir
+    nilai_akhir = hitung_nilai_akhir(nilai_tugas, nilai_uts, nilai_uas)
 
-    nilai_akhir = hitung_nilai_akhir(tugas, uts, uas)
-
+    # Tambahkan data ke dalam list
     data_mahasiswa.append({
-        "Nama": nama,
-        "Tugas": tugas,
-        "UTS": uts,
-        "UAS": uas,
-        "Nilai Akhir": nilai_akhir
+        'Nama': nama,
+        'NIM': nim,
+        'Nilai Tugas': nilai_tugas,
+        'Nilai UTS': nilai_uts,
+        'Nilai UAS': nilai_uas,
+        'Nilai Akhir': nilai_akhir
     })
 
-    jawaban = input("Tambah data lagi? (y/t): ")
-    if jawaban.lower() != 'y':
+    tambah_data = input("\nTambah Data(y/t)? ").lower()
+    if tambah_data != 'y':
         break
 
-print("\nDaftar Data Mahasiswa:")
-for data in data_mahasiswa:
-    print(f"Nama: {data['Nama']}, Tugas: {data['Tugas']}, UTS: {data['UTS']}, UAS: {data['UAS']}, Nilai Akhir: {data['Nilai Akhir']}")
-    
+# Tampilkan output dengan format tabel
+print("\n{:=^65}".format(""))
+print("| No | {:<10} | {:^14} | {:^5} | {:^4} | {:^4} | {:^5} |".format("Nama", "NIM", "Tugas", "UTS", "UAS", "Akhir"))
+print("{:=^65}".format(""))
+
+for idx, mahasiswa in enumerate(data_mahasiswa, start=1):
+    print("| {:<2} | {:<10} | {:^14} | {:^5} | {:^4} | {:^4} | {:.1f} |".format(idx, mahasiswa['Nama'], mahasiswa['NIM'],
+                                                                                   mahasiswa['Nilai Tugas'], mahasiswa['Nilai UTS'],
+                                                                                   mahasiswa['Nilai UAS'], mahasiswa['Nilai Akhir']))
+print("{:=^65}".format(""))
